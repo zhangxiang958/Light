cf2b2f409e9cbbc757a0b56141936b07de769e4c
zhangxiang958 <958033967@qq.com>
2017-04-27 10:13:28 +0800

    create repo

---
+++ index.js
@@ -0,0 +1,17 @@
+(function(win){
+
+    console.log(win);
+
+    var util = {
+
+        /*
+          获取数据类型
+
+          @parameter {*}  检测数据
+          @return {string} 数据类型
+        */
+        
+
+    }
+    
+})(window);
