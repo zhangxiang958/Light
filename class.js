a09b7593df1c4c28461ae20fc3d28980e0d4272e
zhangxiang958 <958033967@qq.com>
2017-07-31 19:36:29 +0800

    done with class

--- class.js
+++ class.js
@@ -13,17 +13,81 @@
 
   }
 
-  Class.prototype.hasClass = function(){
+  Class.prototype.hasClass = (function(){
+    if(document.body.classList) {
+      return function(element, className){
+        if(!element || !className) {
+          return;
+        }
+        className.trim ? className = className.trim() : className = className.replace(/(^\s*)|(\s*$)/g, '');
+        return element.contain(className);
+      }
+    } else {
+      return function(element, className){
+        if(!element || !className) {
+          return;
+        }
+        className.trim ? className = className.trim() : className = className.replace(/(^\s*)|(\s*$)/g, '');
+        return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+      }
+    }
+  }());
 
-  }
+  Class.prototype.addClass = (function(){
+    if(document.body.classList) {
+      return function(element, className){
+        if(!element || !className) {
+          return;
+        }
+        className = className.split(' ');
+        className.forEach(function(name, i){
+          element.classList.add(name);
+        });
+      }
+    } else {
+      return function(element, className){
+        if(!element || !className) {
+          return;
+        }
+        var currentClass = element.className;
+        className = className.split(' ');
+        className.forEach(function(name, i){
+          if(!this.hasClass(element, name)) {
+            currentClass += ' ' + 'name';
+          }
+        });
+        element.className = currentClass;
+      }
+    }
+  }());
 
-  Class.prototype.addClass = function(){
-
-  }
-
-  Class.prototype.removeClass = function(){
-    
-  }
+  Class.prototype.removeClass = (function(){
+    if(document.body.classList) {
+      return function(element, className){
+        if(!element || !className) {
+          return;
+        }
+        className = className.split(' ');
+        className.forEach(function(name, i){
+          element.classList.remove(name);
+        });
+      }
+    } else {
+      return function(element, className){
+        if(!element || !className) {
+          return;
+        }
+        var currentClass = element.className;
+        className = className.split(' ');
+        className.forEach(function(name, i){
+          if(this.hasClass(element, name)) {
+            currentClass.replace(' ' + name + ' ', '');
+          }
+        });
+        element.className = currentClass.trim ? currentClass.trim() : currentClass.replace(/(^\s*)|(\s*$)/g, '');
+      }
+    }
+  }());
 
   return new Class();
 }));
