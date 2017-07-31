d0e6042207e4b25c13bc54c24416468a186e140a
zhangxiang958 <958033967@qq.com>
2017-07-31 10:09:16 +0800

    修改 umd 写法

--- ajax.js
+++ ajax.js
@@ -2,8 +2,8 @@
 (function(root, factory){
 
     if(typeof define === 'function' && define.amd) {
-        define(factory);
-    } else if(typeof exports === 'object'){
+        define([], factory);
+    } else if(typeof module === 'object' && module.exports){
         module.exports = factory();
     } else {
         root.request = factory();
--- css.js
+++ css.js
@@ -1,7 +1,7 @@
 (function(root, factory){
   if(typeof define === 'function' && define.amd) {
-    define(factory);
-  } else if(typeof exports === 'object'){
+    define([], factory);
+  } else if(typeof module === 'object' && module.exports){
     module.exports = factory();
   }else {
     root.css = factory();
