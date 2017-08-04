a9c87e4e6e5e74ca47c11b9db2180df993ecdf89
zhangxiang958 <958033967@qq.com>
2017-08-04 10:51:45 +0800

    add style

--- css.js
+++ css.js
@@ -18,13 +18,27 @@
   function IEVersion(mode){
     return Number(mode);
   }
+  var ieVersion = IEVersion(document.documentMode);
 
   function CSS() {
 
   }
 
-  CSS.prototype.css = function(element, prop, val){
 
+  CSS.prototype.css = function(element, prop, val){
+    if(!element || !prop) {
+      return;
+    }
+
+    if(typeof prop === 'object') {
+      this.setStyle(element, prop, val);
+    } else {
+      if(!!val) {
+        this.setStyle(element, prop, val);
+      } else {
+        this.getStyle(element, prop);
+      }
+    }
   }
 
   CSS.prototype.getStyle = (function(){
@@ -33,43 +47,21 @@
         return (window.getComputedStyle || document.defaultView.getComputedStyle)(element, null).getPropertyValue(prop);
       }
     } else {
-      if(IEVersion(document.documentMode) < 9) {
-        return function(element, prop){
-          prop = camelize(prop);
-          if(prop === 'float') {
-           prop = 'styleFloat'; 
-          }
-          switch(prop) {
-            case 'opacity':
-              try {
-                return element.filters.item('alpha').opacity / 100;
-              }
-              catch (e) {
-                return 1.0;
-              }
-            default:
-              return element.style[prop] || element.currentStyle ? element.currentStyle.getAttribute(prop) : null;
-          }
+      return function(element, prop){
+        prop = camelize(prop);
+        if(prop === 'float') {
+          ieVersion < 9 ? prop = 'styleFloat' : prop = 'cssFloat'; 
         }
-      } else {
-        return function(element, prop){
-          return function(element, prop){
-            prop = camelize(prop);
-            if(prop === 'float') {
-              prop = 'cssFloat'; 
+        switch(prop) {
+          case 'opacity':
+            try {
+              return element.filters.item('alpha').opacity / 100;
             }
-            switch(prop) {
-              case 'opacity':
-                try {
-                  return element.filters.item('alpha').opacity / 100;
-                }
-                catch (e) {
-                  return 1.0;
-                }
-              default:
-                return element.style[prop] || element.currentStyle ? element.currentStyle.getAttribute(prop) : null;
+            catch (e) {
+              return 1.0;
             }
-        }
+          default:
+            return element.style[prop] || element.currentStyle ? element.currentStyle.getAttribute(prop) : null;
         }
       }
     }
@@ -87,8 +79,12 @@
         }
       }
     } else {
-      // styleName = 
-
+      styleName = camelize(styleName);
+      if (styleName === 'opacity' && ieVersion < 9) {
+        element.style.filter = isNaN(val) ? '' : 'alpha(opacity=' + val * 100 + ')';
+      } else {
+        element.style[styleName] = val;
+      }
     }
   }
 
